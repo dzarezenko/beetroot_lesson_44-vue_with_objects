a950221072fab7ed4d5ea5bb3f2044b2c07b5c22
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-13 20:35:29 +0200

    Prototype created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
   <div id="app">
-    
+    <ol>
+      <li v-for="(person, index) in persons" :key="index">{{ person.name }} ({{ person.age }})</li>
+    </ol>
   </div>
 </template>
 
 <script>
+let persons = [
+  {
+    name: "Alex5",
+    age: 5,
+  },
+  {
+    name: "Alex10",
+    age: 10,
+  },
+  {
+    name: "Alex30",
+    age: 30,
+  },
+  
+];
+
 export default {
+  data() {
+    return {
+      persons: persons,
+    }
+  }
 }
 </script>
 
